30efd88372ade851cc1b58074a3a828c8dcd631e
HanBrokenHome <akuntiktok1397@gmail.com>
2024-07-19 08:13:33 +0200

    Upload Pertama

---
+++ vite-project/src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>
+      <input type="text" name="" id="" />
+    </div>
+  )
+}
+
+export default Button
---
+++ vite-project/src/pages/SignIn.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../components/button';
+import Input from '../components/input';
+
+function SignIn() {
+  return (
+    <div>
+      <Input/>
+      <Button/>
+    </div>
+  )
+}
+
+export default SignIn
